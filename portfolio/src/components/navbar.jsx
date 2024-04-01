7855eda9149798597ad06768fa15a2f217ba49bc
nathapon tansit <happy.inprogress@gmail.com>
2024-04-01 22:32:28 +0700

    set up font

--- portfolio/src/components/navbar.jsx
+++ portfolio/src/components/navbar.jsx
@@ -1,3 +1,16 @@
+import smileLogo from "../assets/smile.png";
+
 export default function Navbar() {
-  return <h1>Nav</h1>;
+  return (
+    <div className="flex justify-between items-center h-10 bg-slate-500">
+      <img className="h-6 mx-2" src={smileLogo} />
+      <div className="flex gap-2">
+        <h5>Home</h5>
+        <h5>Projects</h5>
+        <h5>Experience</h5>
+        <h5>Education</h5>
+        <h5>Contacts</h5>
+      </div>
+    </div>
+  );
 }
